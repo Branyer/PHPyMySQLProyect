7d80d9c22e62dff2ba4f6cd6ead7cd22f83031d4
Branyer <branyervergarac@gmail.com>
2020-03-26 19:29:09 -0400

    patterns agregados

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
     <form action="registro.php" method="post">
       <header>Registrase</header>
 
-      <label for="nombre" >Nombres:</label>
-      <input type="text" id="nombre" name="nombre" placeholder="Branyer" maxlength="100" pattern="" required/>
+      <label for="nombre" >Nombre:</label>
+      <input type="text" id="nombre" name="nombre" placeholder="Branyer"   pattern="[a-zA-z]+" required/>
 
-      <label for="apellidos" >Apellidos:</label>
-      <input type="text" id="apellidos" name="apellidos" placeholder="Vergara" maxlength="100" pattern="" required/>
+      <label for="apellidos" >Apellido:</label>
+      <input type="text" id="apellidos" name="apellidos" placeholder="Vergara" pattern="[a-zA-z]+" required/>
 
       <label for="cedula" >Cedula: </label>
-      <input type="text" id="cedula" name="cedula" placeholder="27108704" pattern="" required/>
+      <input type="text" id="cedula" name="cedula" placeholder="27108704" pattern="[0-9]+" required/>
 
       <label for="direccion" >Direccion: </label>
       <input type="text" id="direccion" name="direccion" placeholder="123 calle falsa" required/>
@@ -32,14 +32,23 @@
       <label for="mujer">Mujer</label>
 
       <label for="telefono">Telefono:</label>
-      <input type="text" id="telefono" name="telefono" placeholder="+584140356111"  required/>
+      <input type="tel" id="telefono" name="telefono" placeholder="4140356111"  pattern="[0-9]+" required/>
+
+      <label for="correo">Correo:</label>
+      <input type="email" id="correo" name="correo" placeholder="123@123.com" required/>
 
       <label for="nick">Nickname:</label>
-      <input type="text" id="nick" name="nick"  minlength="5" placeholder="Mi_NickName" maxlength="100" required/>
+      <input type="text" id="nick" name="nick"
+      minlength="5" placeholder="Mi_NickName"
+      maxlength="100"
+      pattern="[A-Za-z0-9_]+"
+      title="No puede contener espacios, ni caracteres especiales."
+      required/>
 
       <label for="password">Contraseña:</label>
       <input type="password" id="password" name="password" minlength="8" maxlength="100" placeholder="mi_contraseña123" required/>
 
+      <input type="submit" value="Enviar datos"/>
 
     </form>
 
